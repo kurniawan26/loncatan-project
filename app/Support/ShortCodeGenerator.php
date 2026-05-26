103294b28ce30a4b4a988e3674942a52b501051a
Fauzi Kurniawan <kirigaya.kurniawan@gmail.com>
2026-05-26 06:03:58 +0000

    feat: add ShortCodeGenerator class for generating unique short codes

---
+++ app/Support/ShortCodeGenerator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Support;
+use App\Models\ShortUrl;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Redis;
+
+class ShortCodeGenerator
+{
+    private const CHARSET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    private const BASE = 62;
+    private const MIN = 62 ** 5; // 62^5 = 916,132,832
+    private const RANGE = 62 ** 6 - self::MIN; // 62^6 - 62^5 = 56,800,235,584
+    private const MULTIPLIER = 2654435761; // A large prime number for hashing
+
+    public function __construct(private readonly int $salt){}
+
+    public function generate(): string
+    {
+        $counter = Redis::incr('short_url_counter');
+        $scrambled = ($counter * self::MULTIPLIER + $this->salt) % self::RANGE;
+        $value = $scrambled + self::MIN;
+
+        return $this->encode($value);
+    }
+
+    private function encode(int $value): string
+    {
+        $shortCode = "";
+        while ($value > 0) {
+            $code = self::CHARSET[$value % self::BASE] . $shortCode;
+            $value = intdiv($value, self::BASE);
+        }
+
+        return $shortCode;
+        
+    }
+}
