c7c53baaedfac81365bd4dd62a8d3447aaa8545f
Fauzi Kurniawan <kirigaya.kurniawan@gmail.com>
2026-05-29 03:54:15 +0000

    feat: addd links index route components

--- resources/js/pages/links/index.tsx
+++ resources/js/pages/links/index.tsx
@@ -1,3 +1,19 @@
-export default function LinksIndex() {
-    return <div />;
+import { Head } from '@inertiajs/react';
+import type { ReactNode } from 'react';
+import AppLayout from '@/layouts/app-layout';
+import type { Paginated, ShortUrl } from '@/types';
+
+interface Props {
+    links: Paginated<ShortUrl>;
 }
+
+export default function LinksIndex({ links }: Props) {
+    return (
+        <>
+            <Head title="My Links" />
+            {/* TODO: implement links table, edit modal, delete confirmation */}
+        </>
+    );
+}
+
+LinksIndex.layout = (page: ReactNode) => <AppLayout>{page}</AppLayout>;
